e4ebf935bd0345c6a5a349aa1ba114ab86aeee35
Letam <davidsonletam@gmail.com>
2025-08-28 23:24:28 +0100

    feat

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ContactSection } from "@/components/ContactSection";
 import { Footer } from "@/components/Footer";
 import { HeroSection } from "@/components/HeroSection";
 import { Navigation } from "@/components/Navigation";
-import { ServicesSection } from "@/components/ServicesSection";
+// import { ServicesSection } from "@/components/ServicesSection";
 import { StaffSection } from "@/components/StaffSection";
 
 export default function Home() {
